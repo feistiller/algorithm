13dda576443ec45e300558b4cbd94f88dbdfbea6
ZTGAME\zhangfan2 <uneedzf@gmail.com>
2017-07-24 19:00:52 +0800

    Revert "第一章"
    This reverts commit 16857af19d037d942c7b350151cbd8b52188efbf.

--- 1/1.2-2.py
+++
@@ -1,7 +0,0 @@
-from sympy import *
-
-n = Symbol('n')
-# 8n^2=64nlgn
-a, b = solve(8 * n ** 2 - 64 * n * log(n, 10), n)
-print N(a)
-print N(b)
--- 1/1.2-3.py
+++
@@ -1,7 +0,0 @@
-from sympy import *
-
-n = Symbol('n')
-# 100n^2=2^n
-a,b = solve(100 * n ** 2 - 2 ** n, n)
-print N(a)
-print N(b)
